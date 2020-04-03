22db2a9914e76ea1e11d631eab75f50d2766205b
mariosknl <mariosknl@outlook.com>
2020-04-03 12:54:49 +0300

    fixed authentication issue

--- lib/checks.rb
+++ lib/checks.rb
@@ -6,7 +6,7 @@ Dotenv.load
 class RetweetBot
   def initialize
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key = ENV['ßCONSUMER_KEY']
+      config.consumer_key = ENV['CONSUMER_KEY']
       config.consumer_secret = ENV['CONSUMER_SECRET']
       config.access_token = ENV['ACCESS_TOKEN']
       config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
